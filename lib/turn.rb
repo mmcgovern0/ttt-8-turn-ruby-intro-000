7ec8038a79365d1b15fb60d79092a4f672f5f486
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-13 17:41:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, player = X)
     display_board(board)
   else
     turn(board)
